9b096cb68b6f50f55730937d7e3b215a0f2dd2a9
Your Name <you@example.com>
2020-09-22 03:51:52 +0300

    change title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Puzzle Solver");
         primaryStage.setScene(new Scene(root, 1600, 1000));
         primaryStage.setFullScreen(true);
         primaryStage.show();
